ddadcc1c876bc34f799f36b87d4f18250c8f4380
RexP10r <ssasnuibul@gmail.com>
2026-05-09 18:23:40 +0300

    feat(nvim): add markdown

--- home/.config/nvim/lua/plugins.lua
+++ home/.config/nvim/lua/plugins.lua
@@ -111,3 +111,14 @@ vim.pack.add({
 require("nvim-autopairs").setup({
 	check_ts = true,
 })
+
+vim.pack.add({
+    'https://github.com/nvim-treesitter/nvim-treesitter',
+    'https://github.com/nvim-mini/mini.nvim',            -- if you use the mini.nvim suite
+    'https://github.com/nvim-mini/mini.icons',        -- if you use standalone mini plugins
+    'https://github.com/nvim-tree/nvim-web-devicons', -- if you prefer nvim-web-devicons
+    'https://github.com/MeanderingProgrammer/render-markdown.nvim',
+})
+require('render-markdown').setup({
+    completions = { lsp = { enabled = true } },
+})
